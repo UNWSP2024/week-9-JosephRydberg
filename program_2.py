e32ef56255d9fa630fcb88d883c5a8fb0a948015
JosephRydberg <98566254+Brrog@users.noreply.github.com>
2024-10-28 15:41:21 -0500

    assignment

--- program_2.py
+++ program_2.py
@@ -3,3 +3,16 @@
 # Each random number should be in the range of 1 through 500. 
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
+from random import randrange
+
+
+def write_numbers():
+    totalNumbers = int(input("How many random numbers do you want? (1000 max) "))
+
+    if totalNumbers <= 1000:
+        file = open("random_numbers", "w")
+        for i in range(0, totalNumbers):
+            file.write(str(randrange(0, 500)) + "\n")
+    else:
+        print("Too large")
+write_numbers()
